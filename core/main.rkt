ea171b9bf4a0f41a77b95e8dcc776a6dcc024d96
Bogdan Popa <bogdan@defn.io>
2022-09-11 19:19:47 +0300

    core: add ConnectionDetails

--- core/main.rkt
+++ core/main.rkt
@@ -1,11 +1,21 @@
 #lang racket/base
 
-(require db
-         noise/backend
-         noise/serde)
+(require noise/backend
+         noise/serde
+         racket/contract
+         racket/string)
 
 (provide
  main)
+
+(define-record ConnectionDetails
+  [(id #f) : (Optional UVarint)]
+  [name : String #:contract non-empty-string?]
+  [(bootstrap-host "127.0.0.1") : String #:contract non-empty-string?]
+  [(bootstrap-port 9092) : UVarint #:contract (integer-in 0 65535)]
+  [(username #f) : (Optional String) #:contract (or/c #f string?)]
+  [(password #f) : (Optional String) #:contract (or/c #f string?)]
+  [(use-ssl #f) : Bool #:contract boolean?])
 
 (define-rpc (hello : String)
   "Hello, world!")
