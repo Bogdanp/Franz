d5c68b3deadfbd625ac85f11abbfd406d9c2b2cc
Bogdan Popa <bogdan@defn.io>
2022-09-26 18:14:18 +0300

    cicia,SidebarEntryCellView: s/partitionsField/countField

--- FranzCocoa/SidebarEntryCellView.swift
+++ FranzCocoa/SidebarEntryCellView.swift
@@ -2,20 +2,20 @@ import Cocoa
 
 class SidebarEntryCellView: NSTableCellView {
   @IBOutlet weak var extendedTextField: NSTextField!
-  @IBOutlet weak var partitionsField: NSTextField!
+  @IBOutlet weak var countField: NSTextField!
 
   func configure(withText text: String) {
     extendedTextField.stringValue = text
     extendedTextField.isHidden = false
     textField?.isHidden = true
-    partitionsField.isHidden = true
+    countField.isHidden = true
   }
 
   func configure(withText text: String, andCount count: String) {
     textField?.stringValue = text
     textField?.isHidden = false
-    partitionsField.stringValue = count
-    partitionsField.isHidden = false
+    countField.stringValue = count
+    countField.isHidden = false
     extendedTextField.isHidden = true
   }
 }
