076806bbfbb4acf1c0125d2306381304b0e4e201
Bogdan Popa <bogdan@defn.io>
2022-10-08 17:32:26 +0300

    cocoa: clean up detail view controller, set constraints

--- FranzCocoa/WorkspaceDetailViewController.swift
+++ FranzCocoa/WorkspaceDetailViewController.swift
@@ -2,6 +2,11 @@ import Cocoa
 import SwiftUI
 
 class WorkspaceDetailViewController: NSViewController {
+  @IBOutlet weak var selectLabel: NSTextField!
+
+  private var contentView: NSView?
+  private var contentViewConstraints: [NSLayoutConstraint]?
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
@@ -9,16 +14,76 @@ class WorkspaceDetailViewController: NSViewController {
   func show(entry: Any, withKind kind: SidebarEntryKind) {
     switch kind {
     case .broker:
-      print("broker=\(entry as! Broker)")
-      let ctl = NSHostingController(rootView: WorkspaceBrokerDetailView(broker: entry as! Broker))
-      children = [ctl]
-      ctl.view.frame = view.bounds
-      for v in view.subviews {
-        v.removeFromSuperview()
-      }
-      view.addSubview(ctl.view)
+      let hostingCtl = NSHostingController(rootView: WorkspaceBrokerDetailView(broker: entry as! Broker))
+      display(controller: hostingCtl)
     default:
-      ()
+      clear()
     }
+  }
+
+  private func display(controller: NSViewController) {
+    selectLabel.isHidden = true
+    contentView?.removeFromSuperviewWithoutNeedingDisplay()
+    if let contentViewConstraints {
+      NSLayoutConstraint.deactivate(contentViewConstraints)
+    }
+    children = [controller]
+    contentView = controller.view
+    contentView?.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(controller.view)
+    contentViewConstraints = fullSizeConstraints(forSubview: controller.view)
+    NSLayoutConstraint.activate(contentViewConstraints!)
+  }
+
+  private func clear() {
+    contentView?.removeFromSuperview()
+    if let contentViewConstraints {
+      NSLayoutConstraint.deactivate(contentViewConstraints)
+    }
+    selectLabel.isHidden = false
+  }
+}
+
+
+// - MARK: NSViewController
+extension NSViewController {
+  func fullSizeConstraints(forSubview subview: NSView) -> [NSLayoutConstraint] {
+    let leading = NSLayoutConstraint(
+      item: subview,
+      attribute: .leading,
+      relatedBy: .equal,
+      toItem: self.view,
+      attribute: .leading,
+      multiplier: 1.0,
+      constant: 0.0
+    )
+    let trailing = NSLayoutConstraint(
+      item: subview,
+      attribute: .trailing,
+      relatedBy: .equal,
+      toItem: self.view,
+      attribute: .trailing,
+      multiplier: 1.0,
+      constant: 0.0
+    )
+    let top = NSLayoutConstraint(
+      item: subview,
+      attribute: .top,
+      relatedBy: .equal,
+      toItem: self.view,
+      attribute: .top,
+      multiplier: 1.0,
+      constant: 0.0
+    )
+    let bottom = NSLayoutConstraint(
+      item: subview,
+      attribute: .bottom,
+      relatedBy: .equal,
+      toItem: self.view,
+      attribute: .bottom,
+      multiplier: 1.0,
+      constant: 0.0
+    )
+    return [leading, trailing, top, bottom]
   }
 }
