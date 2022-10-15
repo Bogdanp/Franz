3f23f7521dc82392ef856757f0df4182fda2c1df
Bogdan Popa <bogdan@defn.io>
2022-10-15 16:50:35 +0300

    cocoa: display version & build numbers in welcome window

--- FranzCocoa/WelcomeWindowContentViewController.swift
+++ FranzCocoa/WelcomeWindowContentViewController.swift
@@ -2,6 +2,16 @@ import Cocoa
 import SwiftUI
 
 class WelcomeWindowContentViewController: NSViewController {
+  @IBOutlet weak var versionLabel: NSTextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") ?? "1"
+    let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") ?? "Unknown"
+    versionLabel.stringValue = "Version \(version) (Build \(build))"
+  }
+
   @IBAction func didPushCloseButton(_ sender: Any) {
     WindowManager.shared.closeWelcomeWindow()
   }
