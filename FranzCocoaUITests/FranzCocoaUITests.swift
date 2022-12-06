7540f7f77900592b3c0220e477e76dd0b4be7b0e
Bogdan Popa <bogdan@defn.io>
2022-12-06 12:56:03 +0200

    test,ui: add license activation test

--- FranzCocoaUITests/FranzCocoaUITests.swift
+++ FranzCocoaUITests/FranzCocoaUITests.swift
@@ -12,16 +12,12 @@ final class FranzCocoaUITests: XCTestCase {
   func testCreateConnection() throws {
     let app = makeUIApp()
     app.launch()
+    try createLocalConnection(app)
 
-    let window = app.windows["Welcome to Franz"]
-    window.buttons["New Connection..."].click()
-    window.sheets.textFields["Connection Name"].typeText("Local")
-    window.sheets.checkBoxes["Enable SSL"].click()
-    window.sheets.buttons["Connect"].click()
     app.windows["Local : 127.0.0.1:9092"].typeKey("w", modifierFlags: .command)
     app.menuBarItems["Window"].menuItems["Welcome to Franz"].click()
     XCTAssertTrue(app.windows["Welcome to Franz"].staticTexts["Local"].exists)
-    window.typeKey("q", modifierFlags: .command)
+    app.windows["Welcome to Franz"].typeKey("q", modifierFlags: .command)
   }
 
   func testCreateDeleteConnection() throws {
@@ -52,10 +48,36 @@ final class FranzCocoaUITests: XCTestCase {
     XCTAssertTrue(window.staticTexts["No Recent Connections"].isHittable)
     window.typeKey("q", modifierFlags:.command)
   }
+
+  func testExpiredTrial() throws {
+    let app = makeUIApp(withDatabasePath: fixture("2022-12-06-expired-trial"))
+    app.launch()
+    try createLocalConnection(app)
+
+    let licenseWindow = app.windows["License"]
+    let licenseField = licenseWindow.groups.children(matching: .textField).element
+    licenseField.click()
+    licenseField.typeText("***REMOVED***")
+    licenseWindow.buttons["Activate License"].click()
+    XCTAssertTrue(licenseWindow.staticTexts["Full Version Activated"].isHittable)
+    licenseWindow.typeKey("q", modifierFlags:.command)
+  }
+
+  private func fixture(_ path: String, ofType ext: String = "sqlite3") -> String {
+    return Bundle(for: type(of: self)).path(forResource: "fixtures/\(path)", ofType: ext)!
+  }
+
+  private func createLocalConnection(_ app: XCUIApplication) throws {
+    let window = app.windows["Welcome to Franz"]
+    window.buttons["New Connection..."].click()
+    window.sheets.textFields["Connection Name"].typeText("Local")
+    window.sheets.checkBoxes["Enable SSL"].click()
+    window.sheets.buttons["Connect"].click()
+  }
 }
 
-func makeUIApp() -> XCUIApplication {
+func makeUIApp(withDatabasePath path: String = "x") -> XCUIApplication {
   let app = XCUIApplication()
-  app.launchEnvironment["FRANZ_TEMP_DATABASE"] = "x"
+  app.launchEnvironment["FRANZ_DATABASE_PATH"] = path
   return app
 }
