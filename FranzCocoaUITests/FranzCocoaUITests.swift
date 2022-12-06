2733cc89d65c9120a7168fe549333f1a40e86657
Bogdan Popa <bogdan@defn.io>
2022-12-06 11:54:37 +0200

    test,ui: add test for create -> delete conn

--- FranzCocoaUITests/FranzCocoaUITests.swift
+++ FranzCocoaUITests/FranzCocoaUITests.swift
@@ -21,7 +21,36 @@ final class FranzCocoaUITests: XCTestCase {
     app.windows["Local : 127.0.0.1:9092"].typeKey("w", modifierFlags: .command)
     app.menuBarItems["Window"].menuItems["Welcome to Franz"].click()
     XCTAssertTrue(app.windows["Welcome to Franz"].staticTexts["Local"].exists)
-    app.windows["Welcome to Franz"].typeKey("q", modifierFlags: .command)
+    window.typeKey("q", modifierFlags: .command)
+  }
+
+  func testCreateDeleteConnection() throws {
+    let app = makeUIApp()
+    app.launch()
+
+    let window = app.windows["Welcome to Franz"]
+    window.buttons["New Connection..."].click()
+
+    let sheet = window.sheets
+    sheet.textFields["Connection Name"].typeText("Bad\t127.0.0.1\t9099")
+    sheet.checkBoxes["Enable SSL"].click()
+    sheet.buttons["Connect"].click()
+
+    let dialog = app.dialogs["alert"]
+    dialog.buttons["OK"].click()
+    app.windows["Bad : 127.0.0.1:9099"].buttons[XCUIIdentifierCloseWindow].click()
+
+    app.menuBarItems["Window"].menuItems["Welcome to Franz"].click()
+    window.tables["Connections Table"]
+      .children(matching: .tableRow)
+      .element(boundBy: 0)
+      .cells.containing(.image, identifier: "Server")
+      .element.rightClick()
+    window.tables["Connections Table"].menuItems["Delete"].click()
+    dialog.buttons["Delete"].click()
+
+    XCTAssertTrue(window.staticTexts["No Recent Connections"].isHittable)
+    window.typeKey("q", modifierFlags:.command)
   }
 }
 
