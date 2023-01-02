a7270bd62a16887cec992ebe7f1f236b81393369
Bogdan Popa <bogdan@defn.io>
2023-01-02 18:57:16 +0200

    build: require kafka-lib 0.4.2+

--- core/info.rkt
+++ core/info.rkt
@@ -10,7 +10,7 @@
                ["deta-lib" #:version "0.11"]
                "http-easy-lib"
                "json-lexer-lib"
-               ["kafka-lib" #:version "0.4.1"]
+               ["kafka-lib" #:version "0.4.2"]
                "libsqlite3"
                "lua-lib"
                "messagepack-lib"
