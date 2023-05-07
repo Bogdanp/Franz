00d1e2705043853124bbe98e5cdd26f43286ac4a
Bogdan Popa <bogdan@defn.io>
2023-05-07 09:39:07 +0300

    build: bump lua-lib to 0.1.2+

--- core/info.rkt
+++ core/info.rkt
@@ -13,7 +13,7 @@
                ["json-lexer-lib" #:version "0.1.1"]
                ["kafka-lib" #:version "0.6"]
                "libsqlite3"
-               ["lua-lib" #:version "0.1.1"]
+               ["lua-lib" #:version "0.1.2"]
                "messagepack-lib"
                ["noise-serde-lib" #:version "0.4"]
                "protocol-buffers-lib"
