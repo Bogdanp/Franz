8f8e54a538b3376a71e2ad1153c312e9047f4ae4
Bogdan Popa <bogdan@defn.io>
2023-06-30 20:14:53 +0300

    core: bump kafka-lib to 0.8

--- core/info.rkt
+++ core/info.rkt
@@ -11,7 +11,7 @@
                ["deta-lib" #:version "0.11"]
                "http-easy-lib"
                ["json-lexer-lib" #:version "0.1.1"]
-               ["kafka-lib" #:version "0.7"]
+               ["kafka-lib" #:version "0.8"]
                "libsqlite3"
                ["lua-lib" #:version "0.1.2"]
                "messagepack-lib"
