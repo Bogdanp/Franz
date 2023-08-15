1f596bf2ddcd40ae52ca3c9a24d4b10e6931ecb6
Bogdan Popa <bogdan@defn.io>
2023-08-15 19:08:18 +0100

    FranzCross,status-bar: fix alignment

--- FranzCross/status-bar.rkt
+++ FranzCross/status-bar.rkt
@@ -17,40 +17,42 @@
    (lambda (dc status)
      (define-values (w h)
        (send dc get-size))
-     (define-values (title-w _title-h _title-baseline _title-extra)
-       (send dc get-text-extent title system-font-s))
-     (define-values (status-w _status-h _status-baseline _status-extra)
-       (send dc get-text-extent status system-font-xs))
-     (p:draw-pict
-      (p:inset
-       (p:lc-superimpose
-        (p:filled-rounded-rectangle
-         #:draw-border? #t
-         #:color (color #xDDDDDDFF)
-         #:border-color (color #xCCCCCCFF)
-         #:border-width 1
-         (- w 10)
-         (- h 7)
-         3 ;radius
-         )
-        (p:inset
-         (p:hc-append
-          (p:colorize
-           (p:text title system-font-s)
-           secondary-color)
-          (p:ghost
-           (p:rectangle
-            (- w 10 4 5 title-w status-w) 0))
-          (p:colorize
-           (p:text status system-font-xs)
-           secondary-color))
-         5 0))
-       5 5)
-      dc 0 0))))
+     (define the-pict
+       (let ([the-pict
+              (p:lc-superimpose
+               (p:filled-rounded-rectangle
+                #:draw-border? #t
+                #:color (color #xDDDDDDFF)
+                #:border-color (color #xCCCCCCFF)
+                #:border-width 1
+                (- w 10)
+                (- h 7)
+                3 ;radius
+                )
+               (p:inset
+                (p:colorize
+                 (p:text title system-font-s)
+                 secondary-color)
+                5 0))])
+         (define status-pict
+           (p:colorize
+            (p:text status system-font-xs)
+            secondary-color))
+         (p:inset
+          (p:pin-over
+           the-pict
+           (- (p:pict-width the-pict)
+              (p:pict-width status-pict)
+              5)
+           (- (/ (p:pict-height the-pict) 2)
+              (/ (p:pict-height status-pict) 2))
+           status-pict)
+          5 5)))
+     (p:draw-pict the-pict dc 0 0))))
 
 (module+ main
   (render
    (window
     #:title "Status Bar"
     #:size '(800 100)
-    (status-bar (@ "Ready") "Example"))))
+    (status-bar (@ "Ready") "Example Connection"))))
