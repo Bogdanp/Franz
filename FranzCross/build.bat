8b251699e47c1cdef971738204e7cf233e4c7020
Bogdan Popa <bogdan@defn.io>
2023-10-08 15:25:11 +0300

    FranzCross,build: make lua accessible at runtime

--- FranzCross/build.bat
+++ FranzCross/build.bat
@@ -1,3 +1,10 @@
 @echo off
-raco exe -o .\Franz.exe --gui --ico .\assets\icon.ico .\main.rkt
+raco exe ^
+  -o .\Franz.exe ^
+  --gui ^
+  --ico .\assets\icon.ico ^
+  ++lang lua ^
+  ++lib lua/lang/runtime-config ^
+  ++lib racket/runtime-config ^
+  .\main.rkt
 raco dist dist .\Franz.exe
