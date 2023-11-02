c14a48ab0979ffca82bd9e4d21b29db33b685aed
Bogdan Popa <bogdan@defn.io>
2023-11-02 20:52:49 +0200

    core,extlib: extract makeChartClass

--- core/extlib/render.lua
+++ core/extlib/render.lua
@@ -1,49 +1,52 @@
 #lang lua
 
--- LineChart -----------------------------------------------------------
+local function makeChartClass(name)
+    local Chart = Class {
+        name = name,
+        constructor = function(xlabel, ylabel)
+            return {
+                __type = name,
+                xlabel = xlabel or "x axis",
+                ylabel = ylabel or "y axis",
+                xs = {},
+                ys = {},
+            }
+        end
+    }
 
-local LineChart = Class {
-    name = "LineChart",
-    constructor = function(xlabel, ylabel)
-        return {
-            __type = "LineChart",
-            xlabel = xlabel or "x axis",
-            ylabel = ylabel or "y axis",
-            xs = {},
-            ys = {},
-        }
+    function Chart:clear()
+        self.xs = {}
+        self.ys = {}
+        return self
     end
-}
 
-function LineChart:clear()
-    self.xs = {}
-    self.ys = {}
-    return self
-end
+    function Chart:push(x, y)
+        table.insert(self.xs, x)
+        table.insert(self.ys, y)
+        return self
+    end
 
-function LineChart:push(x, y)
-    table.insert(self.xs, x)
-    table.insert(self.ys, y)
-    return self
-end
+    function Chart:setxs(xs)
+        self.xs = xs
+        return self
+    end
 
-function LineChart:setxs(xs)
-    self.xs = xs
-    return self
-end
+    function Chart:setys(ys)
+        self.ys = ys
+        return self
+    end
 
-function LineChart:setys(ys)
-    self.ys = ys
-    return self
-end
+    function Chart:__tostring()
+        return string.format(
+            "<%s xlabel=%q ylabel=%q #xs=%d #ys=%d>",
+            name, self.xlabel, self.ylabel, #self.xs, #self.ys
+        )
+    end
 
-function LineChart:__tostring()
-    return string.format(
-        "<LineChart xlabel=%q ylabel=%q>",
-        self.xlabel, self.ylabel
-    )
+    return Chart
 end
 
 return {
-    LineChart = LineChart
+    BarChart = makeChartClass("BarChart"),
+    LineChart = makeChartClass("LineChart")
 }
