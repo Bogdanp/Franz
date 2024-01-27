b68ac02499777b29896af7566742f79c40d1315a
Bogdan Popa <bogdan@defn.io>
2024-01-27 14:29:13 +0200

    core: get version via relative path

--- core/version.rkt
+++ core/version.rkt
@@ -1,14 +1,15 @@
 #lang racket/base
 
 (require (for-syntax racket/base
+                     racket/path
                      setup/getinfo))
 
 (provide
  franz-version)
 
 (begin-for-syntax
-  (define (info-ref id)
-    ((get-info '("franz")) id)))
+  (define this-path (build-path (path-only (syntax-source #'here))))
+  (define info-ref (get-info/full this-path)))
 
 (define-syntax (get-version stx)
   (datum->syntax stx (info-ref 'version)))
