0a2abf0c243042ed65b0f83634787a6f19f14646
Bogdan Popa <bogdan@defn.io>
2025-05-24 16:38:27 +0300

    FranzCross: drop flathub stuff
    It's too much of a hassle to maintain their metadata. Might as well
    just serve the binaries directly.

--- FranzCross/build.sh
+++ FranzCross/build.sh
@@ -7,18 +7,13 @@ raco exe \
   --gui \
   --orig-exe \
   ++aux assets/icon.ico \
-  ++aux flatpak/io.defn.Franz.desktop \
   ++lang lua \
   ++lib lua/lang/runtime-config \
   ++lib racket/runtime-config \
   ./main.rkt
 raco dist dist Franz
 cp assets/icon.svg dist/
-cp flatpak/io.defn.Franz.desktop dist/
-cp flatpak/io.defn.Franz.appdata.xml dist/
 tar cvzf \
     Franz.tar.gz \
     -C dist \
-    bin lib icon.svg \
-    io.defn.Franz.appdata.xml \
-    io.defn.Franz.desktop
+    bin lib icon.svg
