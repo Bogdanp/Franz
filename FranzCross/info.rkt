678c467ce179c1da3b99a065735f3b39fe8d2379
Bogdan Popa <bogdan@defn.io>
2025-08-07 09:52:32 +0300

    core,build: require kafka-lib 0.13+

--- FranzCross/info.rkt
+++ FranzCross/info.rkt
@@ -4,18 +4,19 @@
 (define build-number 1)
 
 (define collection "FranzCross")
-(define deps '("base"
-               "buid-lib"
-               "canvas-list"
-               "db-lib"
-               ["franz" #:version "1.2"]
-               ["gui-easy-lib" #:version "0.15"]
-               "gui-lib"
-               "http-easy-lib"
-               "json-lexer-lib"
-               "net-lib"
-               "pict-lib"
-               "plot-lib"
-               "plot-gui-lib"
-               "threading-lib"))
+(define deps
+  '("base"
+    "buid-lib"
+    "canvas-list"
+    "db-lib"
+    ["franz" #:version "1.2"]
+    ["gui-easy-lib" #:version "0.15"]
+    "gui-lib"
+    "http-easy-lib"
+    "json-lexer-lib"
+    "net-lib"
+    "pict-lib"
+    "plot-lib"
+    "plot-gui-lib"
+    "threading-lib"))
 (define build-deps '("rackunit-lib"))
--- core/info.rkt
+++ core/info.rkt
@@ -2,24 +2,25 @@
 
 (define collection "franz")
 (define version "1.9")
-(define deps '(["amazon-msk-sasl-lib" #:version "0.3"]
-               ["avro-lib" #:version "1.1"]
-               "base"
-               "buid-lib"
-               ["confluent-schema-registry-lib" #:version "0.2.3"]
-               "db-lib"
-               ["deta-lib" #:version "0.11"]
-               "http-easy-lib"
-               ["json-lexer-lib" #:version "0.1.1"]
-               ["kafka-lib" #:version "0.12.1"]
-               ["libsqlite3" #:version "3.43.1"]
-               "libzstd"
-               ["lua-lib" #:version "0.4.1"]
-               "messagepack-lib"
-               ["noise-serde-lib" #:version "0.7"]
-               ["protocol-buffers-lib" #:version "0.1.2"]
-               ["sasl-lib" #:version "1.3"]
-               "snappy-lib"
-               "threading-lib"))
+(define deps
+  '(["amazon-msk-sasl-lib" #:version "0.3"]
+    ["avro-lib" #:version "1.1"]
+    "base"
+    "buid-lib"
+    ["confluent-schema-registry-lib" #:version "0.2.3"]
+    "db-lib"
+    ["deta-lib" #:version "0.11"]
+    "http-easy-lib"
+    ["json-lexer-lib" #:version "0.1.1"]
+    ["kafka-lib" #:version "0.13"]
+    ["libsqlite3" #:version "3.43.1"]
+    "libzstd"
+    ["lua-lib" #:version "0.4.1"]
+    "messagepack-lib"
+    ["noise-serde-lib" #:version "0.7"]
+    ["protocol-buffers-lib" #:version "0.1.2"]
+    ["sasl-lib" #:version "1.3"]
+    "snappy-lib"
+    "threading-lib"))
 (define build-deps '("rackunit-lib"))
 (define compile-omit-paths '("fixtures"))
